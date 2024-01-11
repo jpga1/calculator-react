0db2347e6f80d46a4d2bd6348e8036df45bc22d8
josh k <github.contact@jpa.anonaddy.com>
2024-01-11 16:37:24 -0500

    Add OperationButton component documentation

--- src/OperationButton.jsx
+++ src/OperationButton.jsx
@@ -2,6 +2,7 @@ export default function OperationButton({
   operation,
   handleOperationButtonClick,
 }) {
+  // An operation button component to specify arithmetic operator and click event handler
   return (
     <button
       className="operation-button"
