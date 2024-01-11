f69be55530d034f5f8416c1aa554b26007f9bd8c
josh k <github.contact@jpa.anonaddy.com>
2024-01-11 16:28:08 -0500

    Add App component documentation

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Display from "./Display.jsx";
 import Keypad from "./Keypad.jsx";
 
 export default function App() {
+  // Display and arithmetic states for output of calculations and results
   const [upperDisplay, setUpperDisplay] = useState("");
   const [bottomDisplay, setBottomDisplay] = useState("");
   const [operand1, setOperand1] = useState(0);
@@ -10,10 +11,12 @@ export default function App() {
   const [operation, setOperation] = useState("");
   const [result, setResult] = useState(0);
 
+  // Number button click event handler which updates bottom display with user input
   function handleNumberButtonClick(number) {
     setBottomDisplay((currentInput) => currentInput + number);
   }
 
+  // Operation button click event handler which sets first operand and operator
   function handleOperationButtonClick(operationInput) {
     setOperation(operationInput);
     setOperand1(Number(bottomDisplay));
@@ -25,6 +28,7 @@ export default function App() {
     setUpperDisplay(() => operand1temp + operationInput);
   }
 
+  // Clear button click event handler which resets calculator states
   function handleClearButtonClick() {
     setUpperDisplay("");
     setBottomDisplay("");
@@ -34,12 +38,14 @@ export default function App() {
     setResult(0);
   }
 
+  // Decimal button click event handler which allows for floating point numbers in calculations
   function handleDecimalButtonClick(decimal) {
     setBottomDisplay((currentInput) =>
       currentInput.includes(".") ? currentInput : currentInput + decimal
     );
   }
 
+  // Equals button click event handler which execute calculation
   function handleEqualsButtonClick() {
     setOperand2(Number(bottomDisplay));
 
