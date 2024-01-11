16ddd09a1ed8b60bb30bac2171963402cde0507b
josh k <github.contact@jpa.anonaddy.com>
2024-01-11 16:36:38 -0500

    Add NumberButton component documentation

--- src/NumberButton.jsx
+++ src/NumberButton.jsx
@@ -1,4 +1,5 @@
 export default function NumberButton({ number, handleNumberButtonClick }) {
+  // A numbers button component to specify number value and click event handler
   return (
     <button
       className="number-button"
