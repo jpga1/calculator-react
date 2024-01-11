7bf6fc278a2d7e62f5759e323d6b59bc83ab5efd
josh k <github.contact@jpa.anonaddy.com>
2024-01-11 16:34:15 -0500

    Add Display component documentation

--- src/Display.jsx
+++ src/Display.jsx
@@ -1,4 +1,5 @@
 export default function Display({ upperDisplayValue, bottomDisplayValue }) {
+  // An output component to display user input, the current calculation, and result
   return (
     <div className="output">
       <div className="upper-display">{upperDisplayValue}</div>
